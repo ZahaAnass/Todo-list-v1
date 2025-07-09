dac8209279333a314a9ed72250ea7ca3d3db0fd4
ZahaAnass <zahaanass277@gmail.com>
2025-07-09 15:01:12 +0100

    Updated App.tsx with DragDropContext and onDragEnd implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import InputFeild from "./components/InputFeild/InputFeild";
 import { useState } from "react";
 import { Todo } from "./model";
 import TodoList from "./components/TodoList/TodoList";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import "./App.css";
 
 const App: React.FC = () => {
@@ -25,14 +25,46 @@ const App: React.FC = () => {
     }
   }
 
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+
+    if (source.droppableId === destination.droppableId && source.index === destination.index) {
+      return;
+    }
+    
+    let add: Todo;
+    let active = todos;
+    let completed = completedTodos;
+
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = completed[source.index];
+      completed.splice(source.index, 1);
+    }
+
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
+    } else {
+      completed.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodos(completed);
+    setTodos(active);
+    
+  }
+  
   return(
-    <DragDropContext onDragEnd={() => {}}>
-    <div className="App">
-      <span className="heading">Taskify</span>
-      <InputFeild todo={todo} setTodo={setTodo} handleAdd = {handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos} setCompletedTodos={setCompletedTodos}
-       completedTodos={completedTodos} />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <span className="heading">Taskify</span>
+        <InputFeild todo={todo} setTodo={setTodo} handleAdd = {handleAdd}/>
+        <TodoList todos={todos} setTodos={setTodos} setCompletedTodos={setCompletedTodos}
+          completedTodos={completedTodos} />
+      </div>
     </DragDropContext>
   )
 }
